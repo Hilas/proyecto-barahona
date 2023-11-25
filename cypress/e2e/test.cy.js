51904f4b12bea7cce83778871871c7383bc15ff6
Stefano Viacava <sviacava@talana.com>
2023-11-25 00:12:12 -0300

    intento2

--- cypress/e2e/test.cy.js
+++ cypress/e2e/test.cy.js
@@ -1,7 +1,7 @@
 /// <reference types="Cypress" />
 
 describe('checking things', () => {
-    it('check first thing', () => {
+    it('Get Relaciones', () => {
         cy.visit('https://www.bolsadesantiago.com/resumen_instrumento/CHILE')
 
         cy.get('.card-body > .row > :nth-child(1) > .text-muted').then($elem => {
@@ -27,6 +27,27 @@ describe('checking things', () => {
         cy.get('.card-body > .row > :nth-child(5) > .text-muted').then($elem => {
             const NAV = $elem.text();
             console.log("NAV:", NAV);
+        });
+    });
+
+    it('Get Ultimas variaciones de capital', () => {
+        cy.visit('https://www.bolsadesantiago.com/resumen_instrumento/CHILE')
+
+        const datosTabla = []; // Array para almacenar los datos de cada fila
+
+        cy.get('#price-last-1 > table > tbody > tr').each(($fila) => {
+            const datosFila = {}; // Objeto para almacenar los datos de esta fila
+
+            cy.wrap($fila).find('td').each(($celda, indice) => {
+                cy.wrap($celda).invoke('text').then((textoCelda) => {
+                    // Añade el texto de cada celda al objeto de la fila
+                    datosFila[`Columna ${indice + 1}`] = textoCelda.trim();
+                });
+            }).then(() => {
+                datosTabla.push(datosFila); // Añade los datos de la fila al array de la tabla
+            });
+        }).then(() => {
+            console.table(datosTabla); // Imprime la tabla en la consola
         });
     });
 });
